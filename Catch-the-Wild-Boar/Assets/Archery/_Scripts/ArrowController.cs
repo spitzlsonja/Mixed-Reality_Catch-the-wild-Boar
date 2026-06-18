788b3b78e41be51ab0c28c352df13ace83753735
Vanessa Maurer <s2410892020@fhooe.at>
2026-06-18 10:18:46 +0200

    Archery: Arrow mechanic added

--- Catch-the-Wild-Boar/Assets/Archery/_Scripts/ArrowController.cs
+++ Catch-the-Wild-Boar/Assets/Archery/_Scripts/ArrowController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class ArrowController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject midPointVisual;
+    private GameObject midPointVisual, arrowPrefab, arrowSpawnPoint;
+    
+    [SerializeField]
+    private float arrowMaxSpeed = 10;
 
     public void PrepareArrow()
     {
@@ -15,6 +18,12 @@ public class ArrowController : MonoBehaviour
     public void ReleaseArrow(float strength)
     {
         midPointVisual.SetActive(false);
-        Debug.Log($"Bow strength is {strength}");
+
+        GameObject arrow = Instantiate(arrowPrefab);
+        arrow.transform.position = arrowSpawnPoint.transform.position;
+        arrow.transform.rotation = midPointVisual.transform.rotation;
+        Rigidbody rb = arrow.GetComponent<Rigidbody>();
+        rb.AddForce(midPointVisual.transform.forward * strength * arrowMaxSpeed, ForceMode.Impulse);
+
     }
 }
